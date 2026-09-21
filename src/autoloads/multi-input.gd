4186bb2a6f7abd85d8f7124907b829bd754bf934
igsf <isaelgsfaria@gmail.com>
2026-09-21 17:06:04 -0300

    Add support on MultiInput for multiple keyboards + Negative device IDs reserved to keyboard players

--- src/autoloads/multi-input.gd
+++ src/autoloads/multi-input.gd
@@ -3,11 +3,13 @@
 ## Used to provide simple access to multi device input
 extends Node
 
-const KEYBOARD: int = -1
+# Reserve all negative values to keyboard
+const KEYBOARD_1: int = -1
+const KEYBOARD_2: int = -2
 const MAX_CONTROLLERS_ADDED: int = 8
 
-func _get_kb_action_name(action: StringName) -> String:
-	return action + "@kb"
+func _get_kb_action_name(action: StringName, device: int) -> String:
+	return action + "@kb-" + str(abs(device)) 
 	
 func _get_joy_action_name(action: StringName, device: int) -> String:
 	return action + "@joy-" + str(device)
@@ -15,42 +17,42 @@ func _get_joy_action_name(action: StringName, device: int) -> String:
 func _is_device_controller(device: int) -> bool:
 	return device >= 0
 
-func is_action_just_pressed(action: StringName, device: int = KEYBOARD) -> bool:
+func is_action_just_pressed(action: StringName, device: int = KEYBOARD_1) -> bool:
 		if not _is_device_controller(device): # KEYBOARD
-			action = _get_kb_action_name(action)
+			action = _get_kb_action_name(action, device)
 		else: action = _get_joy_action_name(action, device)
 		return Input.is_action_just_pressed(action)
 		
 		
-func is_action_just_released(action: StringName, device: int = KEYBOARD) -> bool:
+func is_action_just_released(action: StringName, device: int = KEYBOARD_1) -> bool:
 		if not _is_device_controller(device): # KEYBOARD
-			action = _get_kb_action_name(action)
+			action = _get_kb_action_name(action, device)
 		else: action = _get_joy_action_name(action, device)
 		return Input.is_action_just_released(action)
 	
-func is_action_pressed(action: StringName, device: int = KEYBOARD) -> bool:
+func is_action_pressed(action: StringName, device: int = KEYBOARD_1) -> bool:
 		if not _is_device_controller(device): # KEYBOARD
-			action = _get_kb_action_name(action)
+			action = _get_kb_action_name(action, device)
 		else: action = _get_joy_action_name(action, device)
 		return Input.is_action_pressed(action)
 		
 		
-func get_axis(negative: StringName, positive: StringName, device: int = KEYBOARD) -> float:
+func get_axis(negative: StringName, positive: StringName, device: int = KEYBOARD_1) -> float:
 	if not _is_device_controller(device): # KEYBOARD
-		negative = _get_kb_action_name(negative)
-		positive = _get_kb_action_name(positive)
+		negative = _get_kb_action_name(negative, device)
+		positive = _get_kb_action_name(positive, device)
 	else:
 		negative = _get_joy_action_name(negative, device)
 		positive = _get_joy_action_name(positive, device)
 	return Input.get_axis(negative, positive)
 		
 func get_vector(negative_x: StringName, positive_x: StringName,\
- negative_y: StringName, positive_y: StringName, device: int = KEYBOARD) -> Vector2:
+ negative_y: StringName, positive_y: StringName, device: int = KEYBOARD_1) -> Vector2:
 	if not _is_device_controller(device): # KEYBOARD
-		negative_x = _get_kb_action_name(negative_x)
-		positive_x = _get_kb_action_name(positive_x)
-		negative_y = _get_kb_action_name(negative_y)
-		positive_y = _get_kb_action_name(positive_y)
+		negative_x = _get_kb_action_name(negative_x, device)
+		positive_x = _get_kb_action_name(positive_x, device)
+		negative_y = _get_kb_action_name(negative_y, device)
+		positive_y = _get_kb_action_name(positive_y, device)
 	else:
 		negative_x = _get_joy_action_name(negative_x, device)
 		positive_x = _get_joy_action_name(positive_x, device)
